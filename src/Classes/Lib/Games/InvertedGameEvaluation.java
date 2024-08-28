a1169eec0ed1c8fb7e68d965bbf74e1c2a227b1b
Jérémy Munsch <github@jeremydev.ovh>
2024-08-28 11:41:19 +0200

    feat: Add Inverted game
    New Inverted score calculation

---
+++ src/Classes/Lib/Games/InvertedGameEvaluation.java
@@ -0,0 +1,40 @@
+package Classes.Lib.Games;
+
+import Classes.Card;
+import Classes.Interfaces.GameEvaluation;
+import Classes.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class InvertedGameEvaluation implements GameEvaluation {
+
+    private final ArrayList<PlayersCard> tablePlayersCards = new ArrayList<>(4);
+
+    private record PlayersCard(Player player, Card card) {}
+
+    public void resetParticipants() {
+        while (tablePlayersCards.size() > 1) {
+            tablePlayersCards.removeFirst();
+        }
+    }
+
+    public void addParticipant(@NotNull Player player, @NotNull Card card) {
+        tablePlayersCards.add(new PlayersCard(player, player.getCard(0)));
+    }
+
+    public @NotNull Player evaluateWinner() {
+        /* Sort table cards : Compare values */
+        tablePlayersCards.sort((PlayersCard a, PlayersCard b) -> {
+            int cmp = b.card.getValue().value() - a.card.getValue().value();
+            /* If ranks are equals */
+            if (cmp == 0) {
+                /* then sort by colors */
+                return b.card.getColor().value() - a.card.getColor().value();
+            }
+            return cmp;
+        });
+
+        return tablePlayersCards.getLast().player;
+    }
+}
